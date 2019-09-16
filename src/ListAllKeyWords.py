a3d2ead621e1d6d66fa8af531486f4799a63e79b
chwq2001 <chwq2001@sina.com>
2019-09-16 10:55:18 +0800

    src/ListAllKeyWords print current versionb

--- src/ListAllKeyWords.py
+++ src/ListAllKeyWords.py
@@ -1,11 +1,12 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
-import keyword as kw
+import keyword as kw,sys
 last_version_kw = ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
 new_version_added_kw = []
+print('current version is',sys.version)
 for k in kw.kwlist:
     print(k)
     if k not in last_version_kw:
         new_version_added_kw.append(k)
-print(new_version_added_kw)
+print("new_version_added_kw:",new_version_added_kw)
 
