f366ae7b916420e96ffd3dbe989efb614cd21b74
chwq2001 <chwq2001@sina.com>
2019-09-20 14:28:49 +0800

    0920

--- src/test_coroutine/test_async_await.py
+++ src/test_coroutine/test_async_await.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
-import asyncio,threading
+import asyncio,threading,contextlib
 
 
 async def hello():
@@ -26,7 +26,11 @@ print("main")
 
 print("#"*100)
 loop1 = asyncio.get_event_loop()   # actually, this loop is loop above
-tasks = (hello2(), hello2())
-loop1.run_until_complete(asyncio.wait(tasks))
-loop1.close()
+with contextlib.closing(asyncio.get_event_loop()) as loop1:
+    tasks = (hello2(), hello2())
+    loop1.run_until_complete(asyncio.wait(tasks))
+    # loop1.close()
+
+# from Python 3.7, support  asyncio.run，代替 loop1 = asyncio.get_event_loop() ～ loop1.run_until_complete(asyncio.wait(tasks))
+# asyncio.run( asyncio.wait((hello2(), hello2())) )
 print("main2")
---
+++ src/test_coroutine/test_asyncio_2.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+import asyncio
+import aiohttp
+import time
+
+
+async def download_one(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            print('Read {} from {}'.format(resp.content_length, url))
+
+
+async def download_all(sites):
+    tasks = [asyncio.create_task(download_one(site)) for site in sites] # from Python 3.7, support  create_task
+    await asyncio.gather(*tasks)
+
+
+def main():
+    sites = [
+        'http://c.biancheng.net',
+        'http://c.biancheng.net/c',
+        'http://c.biancheng.net/python'
+    ]
+    start_time = time.perf_counter()
+
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(download_all(sites))
+    finally:
+        loop.close()
+    end_time = time.perf_counter()
+    print('Download {} sites in {} seconds'.format(len(sites), end_time - start_time))
+
+
+if __name__ == '__main__':
+    main()
