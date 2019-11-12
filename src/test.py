5beb73fa1d1d4f6eb9e57bccc7f824e6c1a70dbe
chwq2001 <chwq2001@sina.com>
2019-11-12 09:27:34 +0800

    20191112

--- src/test.py
+++ src/test.py
@@ -1,22 +1,11 @@
-import os
-import psutil
 
+class AClass:
+    def __init__(self):
+        pass
 
-# 显示当前 python 程序占用的内存大小
-def show_memory_info(hint):
-    pid = os.getpid()
-    p = psutil.Process(pid)
+    def fn(self,hint):
+        print(str(hint))
 
-    info = p.memory_full_info()
-    memory = info.uss / 1024. / 1024
-    print('{} memory used: {} MB'.format(hint, memory))
-
-
-def func():
-    show_memory_info('initial')
-    a = [i for i in range(10000000)]
-    show_memory_info('after a created')
-
-
-func()
-show_memory_info('finished')
+a = AClass()
+a.fn(2)
+a.fn.__func__(a,22)
